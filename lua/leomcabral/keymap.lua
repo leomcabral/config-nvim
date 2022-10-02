d6e3bcab835e6fd8dd39ff590c53a9f823c7a954
Leonardo Cabral <leomcabral@gmail.com>
2022-10-02 00:12:15 -0300

    add a couple of basic keymaps

--- lua/leomcabral/keymap.lua
+++ lua/leomcabral/keymap.lua
@@ -1,22 +1,28 @@
+local opts = { noremap = true, silent = true }
+
 -- Mappings
-vim.g.mapleader = ' '
 
-local key_mapper = function(mode, key, result)
-  vim.api.nvim_set_keymap(
-    mode,
-    key,
-    result,
-    {noremap = true, silent = true}
-  )
-end
+local keymap = vim.api.nvim_set_keymap
+vim.g.mapleader = " "
+vim.g.localmapleader = " "
 
-key_mapper('', '<up>', '<nop>')
-key_mapper('', '<down>', '<nop>')
-key_mapper('', '<left>', '<nop>')
-key_mapper('', '<right>', '<nop>')
-key_mapper('i', 'jk', '<ESC>')
-key_mapper('i', 'JK', '<ESC>')
-key_mapper('i', 'jK', '<ESC>')
-key_mapper('v', 'jk', '<ESC>')
-key_mapper('v', 'JK', '<ESC>')
-key_mapper('v', 'jK', '<ESC>')
+-- Easier out to normal mode
+keymap("i", "jk", "<ESC>", opts)
+
+-- Buffer navigation
+keymap("n", "<C-]>", ":bnext<CR>", opts)
+keymap("n", "<C-[>", ":bprevious<CR>", opts)
+
+-- Unload buffer
+keymap("n", "<Leader>bk", ":bd<CR>", opts)
+
+-- Tab navigation
+keymap("n", "<tab>", ":tabnext<CR>", opts)
+keymap("n", "<tab>", ":tabnext<CR>", opts)
+
+-- Move line
+keymap("n", "<M-j>", "<ESC>:move .+1<CR>==gi", opts)
+keymap("n", "<M-k>", "<ESC>:move .-2<CR>==gi", opts)
+keymap("v", "<M-j>", ":m .-2<CR>", opts)
+keymap("v", "<M-k>", ":m .-2<CR>", opts)
+
